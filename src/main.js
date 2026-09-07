4739b981d0f119cb01cf99c275950d36d3c5b205
matthewinman0 <matthewinman321@gmail.com>
2026-09-07 20:30:37 +0100

    Introduced Vite

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './style.css'
+import heroImg from './assets/hero.png'
+import javascriptLogo from './assets/javascript.svg'
+import viteLogo from './assets/vite.svg'
+import { setupCounter } from './counter.js'
+
